65c0e0d1f935f84f2496a0b4c7f04084dc707968
SALAVOZ <salik.abdulin@mail.ru>
2021-10-14 02:01:08 +0300

    Добавлен класс для работы с бд

---
+++ iret-sila/Data/AppDBContent.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using iret_sila.Data.Models;
+namespace iret_sila.Data
+{
+    public class AppDBContent : DbContext
+    {
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Song> Songs { get; set; }
+        public DbSet<MusicDirection> MusicDirections { get; set; }
+        public AppDBContent()
+        {
+
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5433;Database=usersdb;Username=postgres;Password=salavat");
+        }
+    }
+}
