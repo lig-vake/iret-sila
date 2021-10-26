dfe4d854f653e6389b57873ab65e3e17bd6c283b
SALAVOZ <salik.abdulin@mail.ru>
2021-10-26 17:51:42 +0300

    Галимый каталог и калимая корзина

---
+++ frontend/src/app/components/catalog/catalog.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-catalog',
+  templateUrl: './catalog.component.html',
+  styleUrls: ['./catalog.component.css']
+})
+export class CatalogComponent implements OnInit {
+  //albom:Albom;
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
+
+}
+
+//interface Albom {
+  //img: string,
+  //name:string,
+  //author: string
+//}
